d1e5f5a1f4cc4017302e00e96a0eb78e043cf04f
Murilo <murilomlssloures@outlook.com>
2022-11-23 15:15:41 -0300

    Projeto concluído

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
